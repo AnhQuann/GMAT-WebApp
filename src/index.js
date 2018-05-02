ba92216cb9858ad06f48be2b8301afce94bb5862
Qk Lahpita <techkids.a12@gmail.com>
2018-05-02 15:41:34 +0700

    2/5

--- src/index.js
+++ src/index.js
@@ -15,52 +15,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-class CompA extends Component {
-    render() {
-        return (
-            <div>ComA</div>
-        )
-    }
-}
-
-class CompB extends Component {
-    render() {
-        return (
-            <div>ComB</div>
-        )
-    }
-}
-class MyLink extends Component {
-    constructor(props) {
-        super(props);
-        this.onClick = this.onClick.bind(this);
-    }
-
-    onClick() {
-        this.props.history.push('/b');
-    }
-
-    render() {
-        return (
-            <div onClick={this.onClick}>Click to change to B</div>
-        )
-    }
-}
-
-const MyLinkWithRouter = withRouter(MyLink);
-
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
-            <div>
-                <Link to='/a'>a</Link>
-                <Link to='/b'>b</Link>
-                <MyLinkWithRouter/>
-                <Switch>
-                    <Route path='/a' component={CompA} />
-                    <Route path='/b' component={CompB} />
-                </Switch>
-            </div>
+            <App/>
         </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
