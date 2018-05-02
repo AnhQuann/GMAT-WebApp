c5285a258069caf9ad00e1ad472a1cf6cfbd8847
huynq <qhuydtvt@gmail.com>
2018-05-02 15:43:34 +0700

    Merged from Qk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Container } from 'reactstrap';
-import { Route, Switch } from 'react-router-dom';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { checkToken } from './actions';
 
@@ -9,32 +10,15 @@ import Login from './components/user/login';
 import Main from './components/main';
 import Dummy from './components/dummy';
 import Loading from './components/common/loading';
+import LecturerContainer from './components/lecturer/LecturerContainer'
 
 class App extends Component {
-  componentWillMount() {
-    this.props.checkToken();
-  }
 
   render() {
     return (
-      <Container>{
-        this.props.authReducer.doneCheckToken
-          ? (this.props.authReducer.isLoggedIn
-            ? (<main>
-              <Switch>
-                <Route exact path="/" component={Main} />
-                <Route path="/dummy" component={Dummy} />
-              </Switch>
-            </main>)
-            : <Login />)
-          : <Loading />
-      }</Container>
+      <LecturerContainer />
     );
   }
 }
 
-function mapStateToProps({ authReducer }) {
-  return { authReducer };
-}
-
-export default connect(mapStateToProps, { checkToken })(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link, withRouter } from 'react-router-dom';
 
 import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import reducers from './reducers';
 
-import App from './app';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <BrowserRouter >
-            <App />
+        <BrowserRouter>
+            <App/>
         </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
