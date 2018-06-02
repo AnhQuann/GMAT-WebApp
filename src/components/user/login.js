35084f70e8defed42324a97d45ce3bab0e69f337
minhduc9699 <minhduc9699@gmail.com>
2018-06-02 16:10:30 +0700

    fix login ui

--- src/components/user/login.js
+++ src/components/user/login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Col, Button, Form, Input, Container, FormGroup } from 'reactstrap';
 
-import { login } from '../../actions';
+import { login } from 'actions';
 
 import EditPanel from '../common/EditPanel';
 
@@ -11,7 +11,6 @@ import Loading from '../common/Loading';
 class Login extends EditPanel {
   constructor(props) {
     super(props);
-    
     this.state = {
       loggingIn: false
     };
@@ -42,25 +41,30 @@ class Login extends EditPanel {
 
   renderLoginForm() {
     return (
-      <Container className="d-flex align-items-center justify-content-center h-100">
-        <Col md="4">
-          <Form onSubmit={this.onSubmit}>
-            <FormGroup>
-              <label>Username</label>
-              <Input onChange={this.inputToProp("values.username")} />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>Password</label>
-              <Input type="password" onChange={this.inputToProp("values.password")}/>
-            </FormGroup>
+      <body className="bg-color">
+        <Container className=" d-flex align-items-center justify-content-center h-100">
+          <Col md="4">
+            <h2 className="login-header text-white"> ILIAT-GMAT </h2>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup className="m-0px">
+                <Input className="input-top"
+                  onChange={this.inputToProp("values.username")}
+                  placeholder="Username" />
+              </FormGroup>
+              <FormGroup>
+                <Input className="input-bot"
+                  type="password"
+                  onChange={this.inputToProp("values.password")}
+                  placeholder="Password" />
+              </FormGroup>
 
-            <FormGroup className="d-flex">
-              <Button className="ml-auto" color="primary">Sign in</Button>
-            </FormGroup>
-          </Form>
-        </Col>
-      </Container>
+              <FormGroup className="d-flex">
+                <Button className="ml-auto button text-dark" >Sign in</Button>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Container>
+      </body>
     );
   }
 }
